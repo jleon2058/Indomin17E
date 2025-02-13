e2b4df12e7920fa12c2c35937ce043366c512794
jleon2058 <arscaballerie@gmail.com>
2025-02-13 05:38:54 -0500

    Se subio el modulo 'purchase_request' , de la 16 se quito el attrs

---
+++ ind_purchase_request/__manifest__.py
@@ -0,0 +1,26 @@
+{
+    "name": "Indomin Purchase Request",
+    "author": "Javier Yataco",
+    'version': "17.0.1.1.0",
+    'depends': ['purchase', 'stock','purchase_request'],
+    'category': 'Purchases',
+    'summary': 'Transferir la distribución analítica de las líneas de pedido a los movimientos de inventario.',
+    'description': """
+        Al confirmar un pedido de compra, transfiere el valor de analytic_distribution
+        desde las líneas de pedido de compra a los movimientos de inventario generados.
+    """,
+    'data':[
+        'security/res_group.xml',
+        'security/purchase_request_security.xml',
+        'security/purchase_order_security.xml',
+        'security/ir.model.access.xml',
+        'reports/report_purchase_request.xml',
+        'reports/purchase_order_report.xml',
+        'views/purchase_request_report.xml',
+        'views/purchase_order.xml',
+        'views/purchase_request.xml',
+        'views/purchase_request_line.xml'
+    ],
+    'installable': True,
+    'application': False,
+}
---
+++ ind_purchase_request/models/__init__.py
@@ -0,0 +1,3 @@
+from . import purchase_order
+from . import purchase_request_line
+from . import purchase_request
