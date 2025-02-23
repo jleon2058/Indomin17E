842b8b6790ab67a18139efaf25ff394b300ad3f0
jleon2058 <arscaballerie@gmail.com>
2025-02-23 06:02:32 -0500

    Se agrego el modulo de Unidad de Medida para colocar codigo SUNAT

---
+++ ind_unidadmedida/__manifest__.py
@@ -0,0 +1,12 @@
+{
+    'name':'Indomin Unidad de medida',
+    'description':'Campos adicionales en la Unidad',
+    'version': '17.0.0.1.0',
+    'author':'Juan Carlos',
+    'depends':[
+        "base","uom"
+    ],
+    'data':[
+        'views.xml'
+    ]
+}
