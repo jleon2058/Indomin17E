84bfd4ec44aa62325984c749f8f1ab7879aad843
jleon2058 <arscaballerie@gmail.com>
2025-02-23 06:03:05 -0500

    Se agrego el modulo del reporte de Kardex Valorado

---
+++ ind_kardex_valorado_general/__manifest__.py
@@ -0,0 +1,16 @@
+{
+    'name':'Indomin Kardex Valorado General',
+    'description':'Reporte de Kardex Valorado a nivel del almacen',
+    'author':'Juan Carlos',
+    'version': '17.0.0.1.0',
+    'depends':[
+        "base","stock","purchase_request","ind_unidadmedida","account","product"
+    ],
+    'data':[
+        "security/res_group.xml",
+        "models/kardexval_general.xml",
+        "security/ir_model_access.xml",
+        "views/stock_move.xml",
+        "views/account_account.xml"
+    ]
+}
