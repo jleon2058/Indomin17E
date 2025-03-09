50f10fc59610ceb0e24004440ecf7ab5afd9007d
jleon2058 <arscaballerie@gmail.com>
2025-03-09 03:56:31 -0500

    Se adapto el 'dv_l10n_pe_account_account' solo el campo serie y numero para v17

--- dv_l10n_pe_account_account/__manifest__.py
+++ dv_l10n_pe_account_account/__manifest__.py
@@ -1,6 +1,6 @@
 {
     'name': """
-        Localización Peruana en Facturación
+        Serie y numero de factura
     """,
 
     'summary': """
@@ -11,38 +11,21 @@
         Agrega campos en facturas de proveedores.
     """,
 
-    'author': 'Develogers',
-    'website': 'https://www.linkedin.com/in/ailton-salguero',
-    'support': 'especialistas@develogers.com',
-    'live_test_url': 'https://wa.me/message/NN37LBBZC5TQA1',
-    'license': 'Other proprietary',
-
+    'author': 'Juan',
     'category': 'Localization',
-    'version': '15.0',
+    'version': '17.0',
     
     'price': 79.99,
     'currency': 'EUR',
 
     'depends': [
         'l10n_latam_invoice_document',
-        'l10n_pe',
-        'dv_l10n_pe_edi_table',
-        'dv_account_invoice_date_currency_rate',
+        'l10n_pe'
     ],
 
     'data': [
-        'views/account_account_views.xml',
-        'views/account_move_views.xml',
-        'views/account_move_line_views.xml',
-        'views/l10n_latam_identification_type_views.xml',
-        'views/product_template_views.xml',
-        'views/menu_item_views.xml',
-        'data/res.partner.csv',
-        'data/l10n_latam.document.type.csv',
-        #'data/account_tax_data.xml',
+        'views/account_move.xml'
     ],
-    
-    'images': ['static/description/banner.gif'],
     
     'auto_install': False,
 	'application': True,
